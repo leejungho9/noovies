43f81b4ce06b4c3e9df7c87f17a241f1253f6ebb
unknown <9wjdgh9@naver.com>
2023-10-13 02:02:46 +0900

    #2.14 styled-components

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,11 +1,39 @@
 import { Text, TouchableOpacity, View } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: blue;
+`;
+
+const Header = styled.View``;
+const Column = styled.View``;
+const Footer = styled.View``;
 
 const Movies = ({ navigation: { navigate } }) => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <TouchableOpacity onPress={() => navigate("Stack", { screen: "Three" })}>
-      <Text>Movies</Text>
-    </TouchableOpacity>
-  </View>
+  <Header>
+    <Column>
+      <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+        <Title>Movies</Title>
+      </Btn>
+    </Column>
+    <Footer></Footer>
+  </Header>
 );
+
+// const styles = StyleSheet.create({
+//   btn: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
+// ! 아직 js 코드 , 우리는 css 코드를 작성하는데 익숙하기 때문에 어려움이 있음
+// ! align-items 의 경우 alignItems 로 작성해야함, 이를 해결하기 위해 styled-component 사용
 
 export default Movies;
